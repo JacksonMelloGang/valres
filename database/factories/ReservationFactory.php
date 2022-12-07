3424be575028c1109fd24df64d53ba6b3be26f25
Nima <50780747+JacksonMelloGang@users.noreply.github.com>
2022-12-08 00:10:51 +0100

    fixed reservationfactory
    Acked-by: Askyna <sio.morinl@bts-sio.ozenne.fr>

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -3,17 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
 class ReservationFactory extends Factory
 {
-
-
     protected $model = \App\Models\Reservation::class;
-
 
     /**
      * Define the model's default state.
@@ -23,25 +19,15 @@ class ReservationFactory extends Factory
     public function definition()
     {
         return [
-            'utilisateur_id' => fake()->numberBetween(1, 7),
-            'salle_id' => fake()->numberBetween(1, 10),
-            'date_reservation' => fake()->date(),
-            'reservation_periode' => fake()->numberBetween(1, 4),
-            'reservation_commentaire' => fake()->sentence(),
+            //
+            'utilisateur_id' => $this->faker->numberBetween(1, 7),
+            'salle_id' => $this->faker->numberBetween(1, 10),
+            'date_reservation' => $this->faker->date(),
+            'reservation_periode' => $this->faker->numberBetween(1, 3),
+            'reservation_commentaire' => $this->faker->text(100),
+            'reservation_statut' => $this->faker->numberBetween(1, 3),
+
+
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            //'email_verified_at' => null,
-        ]);
-    }
-
-
 }
