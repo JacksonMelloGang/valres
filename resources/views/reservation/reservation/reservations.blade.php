beb12d6c0a3465798baa58d2853b9841814b9a62
JacksonMellogang <50780747+JacksonMelloGang@users.noreply.github.com>
2023-01-04 13:51:55 +0100

    need toggle button to show either a table or a calendar

--- resources/views/reservation/reservation/reservations.blade.php
+++ resources/views/reservation/reservation/reservations.blade.php
@@ -17,57 +17,33 @@
         </div>
     @endif
 
-    <table class="table table-striped table-secondary">
+    <table class="table">
         <thead>
-                <th scope="col"></th>
-            @foreach($salles as $salle)
-                <th scope="col">{{$salle->salle_nom}}</th>
-            @endforeach
+            <tr>
+                <th>Heures</th>
+                @foreach ($salles as $salle)
+                    <th>{{ $salle->salle_nom }}</th>
+                @endforeach
+            </tr>
         </thead>
         <tbody>
-        @php($heure = ['8h00', '9h00', '10h00', '11h00', '12h00', '13h00', '14h00', '15h00', '16h00', '17h00', '18h00', '19h00', '20h00', '21h00', '22h00', '23h00'])
-        @foreach($heure as $h)
+            @for ($i = 1; $i <= 4; $i++)
                 <tr>
-                    <th scope="row">{{$h}}</th>
-                    @foreach($salles as $salle)
+                    <td>{{ $i }}</td>
+                    @foreach ($salles as $salle)
                         <td>
-                            @foreach($reservations as $reservation)
-                                @if($reservation->salle_id == $salle->salle_id && $reservation->date_reservation == $date)
-                                    @php($i = 0)
-                                    @while($i < 25)
-                                        @switch($reservation->reservation_periode)
-
-                                                @case(1)
-                                                    @if($i >= 8 && $i < 12)
-                                                        <div class="bg-danger">
-                                                            <a href="" class="alert-link">Occupé</a>
-                                                        </div>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
-                                                @break
-                                                @case(2)
-                                                    @if($i >= 8 && $i < 12)
-                                                        <div class="bg-danger">
-                                                            <a href="" class="alert-link">Occupé</a>
-                                                        </div>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
-                                                @break
-                                                @case(3)
-                                                    @if($i >= 8 && $i < 12)
-                                                        <div class="bg-danger">
-                                                            <a href="" class="alert-link">Occupé</a>
-                                                        </div>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
-                                                @break
-                                            @endswitch
-
+                            @foreach ($salle->reservations as $reservation)
+                                @if ($reservation->periode->id_rsperiode == $i)
+                                    <td class="bg-danger">AAAA</td>
+                                @else
+                                    <td class="bg-success"></td>
+                                @endif
+                            @endforeach
                         </td>
+                    @endforeach
                 </tr>
+            @endfor
         </tbody>
     </table>
+
 @endsection
